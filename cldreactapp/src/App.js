a66ed7578600de851e3fa66433e0c7ff1c37ad84
Tanya345 <86947320+Tanya345@users.noreply.github.com>
2021-09-29 20:31:23 +0530

    Add files via upload

---
+++ cldreactapp/src/App.js
@@ -0,0 +1,56 @@
+import { Header } from "./MyComponents/Header";
+import { Colleges } from "./MyComponents/Colleges";
+import './App.css';
+import data from './colleges.json';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <Header />
+    
+        <div className="mygrid" >
+        {data.colleges.map(clg =>{
+       return(
+         <div className="ptclg" key={clg.id}>
+         
+          <div className="clg" >
+            <div className="coldiv">
+              <div className="imgdiv"></div>
+
+            </div>
+            <p id="flag"></p>
+            <p id="promote">Promoted</p>
+            <p className="rate"><b>{clg.rating}</b>/5<br />{clg.rating_remarks}</p>
+            <p className="txt1">{clg.tags[0]}</p>
+            <p className="txt2">{clg.tags[1]}</p>
+            <p className="txt3">{clg.ranking}</p>
+
+            <div className="pDiv">
+              <div className="txtDivLeft">
+                <p className="clgName">{clg.college_name}</p>
+                <p className="add1">{clg.nearest_place}</p>
+                <p className="add2">{clg.famous_nearest_places}</p>
+                <p className="text">{clg.offertext}</p>
+              </div>
+              <div className="txtDivRight">
+                <p className="origFees">&#8377;<strike>{clg.original_fees}    </strike><div id="disc"></div></p>
+                <p className="discount">{clg.discount}</p>
+                <p className="disFees">&#8377;{clg.discounted_fees}</p>
+                <p className="txt4">{clg.fees_cycle}</p>
+                <p className="txt5">{clg.amenties}</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+           )
+          })}
+
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
